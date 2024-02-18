b9073f16ae8e7c235fadbaf34dcd169954466961
aman <asaxena7531@gmail.com>
2024-02-18 20:20:14 +0530

    vercel deploy

--- EchoReferral/settings.py
+++ EchoReferral/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-11d%4mo8_h%6-pe+oiqw$umbk+yncl4y%f@nd_pi$i*x9p74^5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
